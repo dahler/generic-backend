383db93d7f9496ca4655a27d3d942cc4ec76ebf8
Syahrial Dahler <dahler@gmail.com>
2025-01-22 07:43:17 +0700

    init build faiss -1

--- DBManager/__init__.py
+++ DBManager/__init__.py
@@ -5,7 +5,11 @@ from .Models import Base
 from config.config import db_url
 
 # print(db_url)
-engine = create_engine(db_url)
+engine = create_engine(db_url,
+                    pool_size=5,  # Number of connections in the pool
+                    max_overflow=10,  # Extra connections beyond the pool size
+                    pool_timeout=30,  # Seconds to wait before timing out   
+                       )
 
 # Create a configured "Session" class
 Session = scoped_session(sessionmaker(bind=engine))
